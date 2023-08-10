9fc306960fb25026b33cd4dae76c466176487469
trevorism <trevorism@gmail.com>
2023-08-10 12:36:27 -0400

    Change rpc

--- src/main/groovy/com/trevorism/service/FlareApiType.groovy
+++ src/main/groovy/com/trevorism/service/FlareApiType.groovy
@@ -5,7 +5,7 @@ class FlareApiType {
     static FlareApiType BITCOIN = new FlareApiType("bitcoin", "https://api.flare.network/bitcoin")
     static FlareApiType LITECOIN = new FlareApiType("litecoin", "https://api.flare.network/litecoin")
     static FlareApiType XRPL = new FlareApiType("xrpl", "https://api.flare.network/xrpl")
-    static FlareApiType FLARE = new FlareApiType("flare", "https://api.flare.network/flare")
+    static FlareApiType FLARE = new FlareApiType("flare", "https://api.flare.network/flare/bc/C/rpc")
     static FlareApiType SONGBIRD = new FlareApiType("songbird", "https://api.flare.network/songbird")
     static FlareApiType FLARE_EXPLORER = new FlareApiType("flare-explorer", "https://api.flare.network/flare-explorer")
 
