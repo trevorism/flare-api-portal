2e312d373879d4e87ef18ddaf84381381d45819c
Trevor Brooks <trevorism@gmail.com>
2024-05-27 09:43:25 -0400

    UPdate dependencies and bump version

--- src/main/groovy/com/trevorism/Application.groovy
+++ src/main/groovy/com/trevorism/Application.groovy
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.info.Info
 @OpenAPIDefinition(
         info = @Info(
                 title = "Flare API Portal",
-                version = "0.2.0",
+                version = "0.3.0",
                 description = "Wraps the Flare API for Trevorism",
                 contact = @Contact(url = "https://trevorism.com", name = "Trevor Brooks", email = "tbrooks@trevorism.com")
         )
